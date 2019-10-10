21fe9167f34f84e6f70152ddf5a48eb228487334
zhanggq <guoqing_life@163.com>
2019-10-10 18:07:07 +0800

    添加@Repository,这样注入时IDEA不报错

--- src/main/java/com/card/zh/mapper/SeqnoMapper.java
+++ src/main/java/com/card/zh/mapper/SeqnoMapper.java
@@ -3,8 +3,10 @@ package com.card.zh.mapper;
 import com.card.zh.entity.Seqno;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface SeqnoMapper {
 
     Seqno querySeqNo(@Param("seqType") String seqType, @Param("genDate") String genDate);
