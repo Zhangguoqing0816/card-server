0c1dda9e9327ba5d0b75dc8c3ebd6d001e250ad6
zhanggq <guoqing_life@163.com>
2019-10-10 10:57:25 +0800

    代码生成器

---
+++ src/main/java/com/card/zh/util/CodeGeneration.java
@@ -0,0 +1,80 @@
+package com.card.zh.util;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * @Auther:
+ * @Description: 代码生成器
+ * @Date: 2019/10/10 10:11
+ */
+public class CodeGeneration {
+
+    /**
+     * @param args
+     * @Title: main
+     * @Description: 生成
+     */
+    public static void main(String[] args) {
+        //传参数据库表名称
+        String tableName = "temp";
+        generationCode(tableName);
+    }
+
+    public static void generationCode(String tableName) {
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir("C://codeGeneration");
+//        gc.setOutputDir(System.getProperty("user.dir")+"/src/main/java/");
+        gc.setFileOverride(true);
+        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+        gc.setAuthor("zhanggq");// 作者
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setControllerName("%sAction");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        mpg.setGlobalConfig(gc);
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("heq");
+        dsc.setPassword("123456");
+        dsc.setUrl("jdbc:mysql://118.24.152.121:3306/card");
+        mpg.setDataSource(dsc);
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+//        strategy.setTablePrefix(new String[] { "sys_" });// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(new String[]{tableName}); // 需要生成的表
+        strategy.setSuperServiceClass(null);
+        strategy.setSuperServiceImplClass(null);
+        strategy.setSuperMapperClass(null);
+        mpg.setStrategy(strategy);
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.card.zh");
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("Impl");
+        pc.setMapper("mapper");
+        pc.setEntity("entity");
+        pc.setXml("xml");
+        mpg.setPackageInfo(pc);
+
+        // 执行生成
+        mpg.execute();
+    }
+
+}
