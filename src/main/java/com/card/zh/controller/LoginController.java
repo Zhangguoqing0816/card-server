1af6efbd2f3d376babd54df2fa994566f89e7707
heqiang <1253312582@qq.com>
2019-10-17 12:43:32 +0800

    配置文件分割

--- src/main/java/com/card/zh/controller/LoginController.java
+++ src/main/java/com/card/zh/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.card.zh.model.session.SessionModel;
 import com.card.zh.service.LoginService;
 import com.card.zh.util.ResultData;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -27,12 +28,14 @@ public class LoginController {
 
     @GetMapping("/patchca")
     @ApiOperation(value = "获取验证码", notes = "获取验证码")
+    @ApiImplicitParam(name="request",value = "验证码请求对象",required = true,dataTypeClass =HttpServletResponse.class)
     public void patchca(HttpServletRequest request, HttpServletResponse response) throws Exception {
         loginService.patchca(request, response);
     }
 
     @PostMapping("/login")
     @ApiOperation(value = "用户登陆", notes = "用户登录")
+    @ApiImplicitParam(name="loginRequest",value = "登录用户",required = true,dataTypeClass= LoginRequest.class)
     public ResultData login(@RequestBody LoginRequest loginRequest) {
         String result = loginService.loginCheckInfo(loginRequest);
         if (result.contains("ok")) {
@@ -45,6 +48,7 @@ public class LoginController {
 
     @PostMapping("/updatePassword")
     @ApiOperation(value = "修改密码", notes = "修改密码")
+    @ApiImplicitParam(name="loginRequest",value = "用户的密码信息",required = true,dataTypeClass= LoginRequest.class)
     public ResultData update(@RequestBody LoginRequest loginRequest) {
         String result = loginService.updatePassword(loginRequest);
         if (result.contains("ok")) {
